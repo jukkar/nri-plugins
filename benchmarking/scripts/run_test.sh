193a3865fddcd6894d57a03f15763f54fc7a626b
Ville Pihlava <ville.pihlava@intel.com>
2023-03-16 15:19:11 +0200

    Small changes.

--- benchmarking/scripts/run_test.sh
+++ benchmarking/scripts/run_test.sh
@@ -7,19 +7,23 @@ BASE_DIR="$(realpath "${SCRIPT_DIR}/..")"
 
 NUMBER_OF_CONTAINERS_IN_INCREMENT="1"
 NUMBER_OF_INCREMENTS="1"
+FILENAME_LABEL=""
 
 usage () {
     echo "usage: $0 -n <number of stress-ng containers in increment> -i <increments>"
     exit 1
 }
 
-while getopts ":n:i:" option; do
+while getopts ":n:i:l:" option; do
     case ${option} in
         n) 
             NUMBER_OF_CONTAINERS_IN_INCREMENT="${OPTARG}"
             ;;
         i)
             NUMBER_OF_INCREMENTS="${OPTARG}"
+            ;;
+        l)
+            FILENAME_LABEL="${OPTARG}"
             ;;
         \?)
             usage
@@ -56,5 +60,13 @@ END_TIME=$(date +%s)
 sleep 15s
 
 OUTPUT_FILE_DATE_PREFIX=$(date -u +"%Y%m%dT%H%M%SZ" -d "@${START_TIME}")
-python3 get-prometheus-timeseries-data.py http://127.0.0.1:30000 -q "rate(container_cpu_usage_seconds_total{container=\"nri-resmgr-topology-aware\"}[1m])" -s "${START_TIME}" -e "${END_TIME}" -c "${BASE_DIR}/output/${OUTPUT_FILE_DATE_PREFIX}-prometheus.csv"
-python3 get-jaeger-tracing-data.py http://127.0.0.1:16686 -c "${BASE_DIR}/output/${OUTPUT_FILE_DATE_PREFIX}-jaeger.csv"
+OUTPUT_FILE_PREFIX="${OUTPUT_FILE_DATE_PREFIX}-${FILENAME_LABEL}"
+
+# Check if Prometheus is used
+if nc -z 127.0.0.1 30000; then
+    python3 get-prometheus-timeseries-data.py http://127.0.0.1:30000 -q "rate(container_cpu_usage_seconds_total{container=\"nri-resmgr-topology-aware\"}[1m])" -s "${START_TIME}" -e "${END_TIME}" -c "${BASE_DIR}/output/${OUTPUT_FILE_PREFIX}-prometheus-rate-container_cpu_usage_seconds_total.csv"
+    python3 get-prometheus-timeseries-data.py http://127.0.0.1:30000 -q "container_memory_usage_bytes{container=\"nri-resmgr-topology-aware\"}" -s "${START_TIME}" -e "${END_TIME}" -c "${BASE_DIR}/output/${OUTPUT_FILE_PREFIX}-prometheus-container_memory_usage_bytes.csv"
+    python3 get-prometheus-timeseries-data.py http://127.0.0.1:30000 -q "container_memory_working_set_bytes{container=\"nri-resmgr-topology-aware\"}" -s "${START_TIME}" -e "${END_TIME}" -c "${BASE_DIR}/output/${OUTPUT_FILE_PREFIX}-prometheus-container_memory_working_set_bytes.csv"
+fi
+
+python3 get-jaeger-tracing-data.py http://127.0.0.1:16686 -c "${BASE_DIR}/output/${OUTPUT_FILE_PREFIX}-jaeger.csv"
