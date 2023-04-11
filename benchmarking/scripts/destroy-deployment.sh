976d3c4aedde941767816b3e63f4a524247fd47f
Ville Pihlava <ville.pihlava@intel.com>
2023-04-11 12:12:30 +0300

    Change jaeger to Kubernetes deployment.

--- benchmarking/scripts/destroy-deployment.sh
+++ benchmarking/scripts/destroy-deployment.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 BASE_DIR="$(realpath "${SCRIPT_DIR}/..")"
 
 kubectl delete -f "${BASE_DIR}/manifests/stress-ng-deployment.yaml"
-
+kubectl delete -f "${BASE_DIR}/manifests/jaeger-deployment.yaml"
 helm uninstall -n monitoring prometheus
