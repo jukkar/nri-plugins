12415d4bfac32611fe0def1562817876820edef5
Ville Pihlava <ville.pihlava@intel.com>
2023-04-13 10:55:51 +0300

    Add changes for running tests with template policy.

--- benchmarking/scripts/post_run.sh
+++ benchmarking/scripts/post_run.sh
@@ -5,5 +5,5 @@
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 BASE_DIR="$(realpath "${SCRIPT_DIR}/..")"
 
-python3 plot-graphs.py -o "${BASE_DIR}/output/traces.png" -l "topology-aware-jaeger,baseline-jaeger" "${BASE_DIR}/output"
-python3 plot-graphs.py -o "${BASE_DIR}/output/resource_usage.png" -l "topology-aware-prometheus" "${BASE_DIR}/output"
+python3 plot-graphs.py -o "${BASE_DIR}/output/traces.png" -l "topology-aware-jaeger,template-jaeger,baseline-jaeger" "${BASE_DIR}/output"
+python3 plot-graphs.py -o "${BASE_DIR}/output/resource_usage.png" -l "topology-aware-prometheus,template-prometheus" "${BASE_DIR}/output"
